82048de8ea3baae7f0f76bb7ba7078f12c3dafa2
SarkerTech <hrid.sarker@yahoo.com>
2026-09-08 22:12:58 -0400

    Implement security analyzer

--- analyzer.py
+++ analyzer.py
@@ -1,1 +1,19 @@
 
+def analyze(logs):
+    findings = []
+
+    for event in logs["Records"]:
+
+        if event["eventName"] == "CreateAccessKey":
+            findings.append({
+                "severity": "High",
+                "finding": "Access Key Created"
+            })
+
+        if event["eventName"] == "TerminateInstances":
+            findings.append({
+                "severity": "Medium",
+                "finding": "EC2 Instance Termination"
+            })
+
+    return findings
